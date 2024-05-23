ddf22b6bec8d9d8523f6aa44b7e0f54810a9fc41
Zoselro <zoselro0328@gmail.com>
2024-05-23 16:06:03 +0900

    dev-rebase2

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 //index.html 을 위한 js file
-function login(username){
-    console.log(username);
+function login(username,pwd){
+    if(username) console.log(username);
+    if(pwd) console.log(pwd);
 };
 
 function logout(){
     console.log("logout");
 };
+
+function rebase_feat1(){
+    console.log("rebase_feat1");
+}
+
+function rebase_feat2(){
+    console.log("rebase_feat2");
+}
+
