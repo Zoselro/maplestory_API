bc60f88534d6319d0aa176c28b799f1e0fd4edfb
Zoselro <zoselro0328@gmail.com>
2024-10-24 15:00:19 +0900

    2024-10-24 15:00 theseed_ranking added

--- Character_Data/Character_theseed_data.py
+++ Character_Data/Character_theseed_data.py
@@ -1,16 +1,15 @@
 import requests
 import json
 import headers_data
-#캐릭터가 2023년 12월 22일부터 datetime.now() 까지 더 시드 기록이 있는 것만 조회 한 후 가장 높은 층수를 기록하고 있는 데이터를 json 데이터 출력 후 json에 저장
 
-file_path = r'D:\Project\python\Character_Data_json\maplestory_api_character_dojang_data.json'    
-def get_character_dojang(world_name, difficulty, job, ocid, page_num, api_key, date_value):
+file_path = r'D:\Project\python\Character_Data_json\maplestory_api_character_theseed_data.json'    
+def get_character_theseed(world_name, ocid, page_num, api_key, date_value):
     
     headers = headers_data.headers_data(api_key)
-    urlString = f"https://open.api.nexon.com/maplestory/v1/ranking/dojang?date={date_value}&world_name={world_name}&difficulty={difficulty}&class={job}&ocid={ocid}&page={page_num}"
+    urlString = f"https://open.api.nexon.com/maplestory/v1/ranking/theseed?date={date_value}&world_name={world_name}&ocid={ocid}&page={page_num}"
     response = requests.get(urlString, headers = headers)
-    
     data = response.json()
+    
     data_value = data.get('ranking')
     data_all = []
     
--- Character_Data/main.py
+++ Character_Data/main.py
@@ -2,10 +2,12 @@ import Character_basic_data
 import Character_ocid
 import Character_dojang_data
 import headers_data
+import Character_theseed_data
+
 from datetime import datetime, timedelta #파라미터로 날짜를 받기위함
 
 api_key = "live_454c2b1ff9fd60b4ab2ee265c9f236ba3dfb7f486da0b6c3f76999ce002754e2efe8d04e6d233bd35cf2fabdeb93fb0d"
-character_name = "팬슈"
+character_name = "도난"
 world_name = "리부트"
 difficulty = "1" #통달
 job = "팬텀-전체 전직" #직업
@@ -25,23 +27,32 @@ start_date = datetime(2023, 12, 22)
 end_date = datetime.now()
 current_date = start_date
 
-max_dojang_floor = 0 #가장 높은 층수
-
+max_dojang_floor = 0 #무릉도장 가장 높은 층수
+max_theseed_floor = 0 # 더시드 가장 높은 층수
 #무릉도장 json 파일 초기화
 with open(Character_dojang_data.file_path, 'w', encoding='utf-8') as json_file:
     json_file.write('')
+#더 시드 json 파일 초기화
+with open(Character_theseed_data.file_path, 'w', encoding='utf-8') as json_file:
+    json_file.write('')
 
 while current_date <= end_date:
-    date_value = current_date.strftime('%Y-%m-%d')
-    
+    date_value = current_date.strftime('%Y-%m-%d')   
      # 각 날짜에 대해 무릉 데이터를 조회 및 저장
     dojang_data = Character_dojang_data.get_character_dojang(world_name, difficulty, job, ocid, page, api_key, date_value)
+    theseed_data = Character_theseed_data.get_character_theseed(world_name, ocid, page, api_key, date_value)
     current_date += timedelta(days=1)
     
     if dojang_data:
-        for entry in dojang_data:
-            print(f"날짜: {entry['date']}, 무릉 층수: {entry['dojang_floor']}")
-            if entry['dojang_floor'] > max_dojang_floor:
-                max_dojang_floor = entry['dojang_floor']
-                
+        for entry_dojang in dojang_data:
+            #print(f"날짜: {entry['date']}, 무릉 층수: {entry['dojang_floor']}")
+            if entry_dojang['dojang_floor'] > max_dojang_floor:
+                max_dojang_floor = entry_dojang['dojang_floor']
+    
+    if theseed_data:
+        for entry_theseed in theseed_data:
+            if entry_theseed['theseed_floor'] > max_theseed_floor:
+                max_theseed_floor = entry_theseed['theseed_floor']
+    
 print(f"가장 높은 무릉 층수: {max_dojang_floor}")
+print(f"가장 높은 더시드 층수: {max_theseed_floor}")
