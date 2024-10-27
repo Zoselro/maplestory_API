b3103a40cf3ac0d4e59373be3d5a3b4e55a6afe6
Zoselro <zoselro0328@gmail.com>
2024-10-27 22:27:56 +0900

    20241027 22:27 commit

--- Character_Data_cache_async/Union_Character_List.py
+++ Character_Data_cache_async/Union_Character_List.py
@@ -10,15 +10,17 @@ def Union_Character_list(api_key, ocid, date_value):
     json_file_clear.initialize_json_file(file_path)
     
     headers = headers_data.headers_data(api_key)
-    urlString = f"https://open.api.nexon.com/maplestory/v1/user/union-raider?ocid={ocid}&date={date_value}"
-    print(urlString)
+    #urlString = f"https://open.api.nexon.com/maplestory/v1/user/union?ocid={ocid}&date={date_value}" #유니온 정보 조회
+    #urlString = f"https://open.api.nexon.com/maplestory/v1/user/union-raider?ocid={ocid}&date={date_value}" #유니온 공격대 정보 조회
+    #urlString = f"https://open.api.nexon.com/maplestory/v1/user/union-artifact?ocid={ocid}&date={date_value}" #유니온 아티팩트 정보 조회
+    urlString = f"https://open.api.nexon.com/maplestory/v1/ranking/union?ocid={ocid}&date={date_value}" #유니온 랭킹 정보 조회 (본캐 조회)
     response = requests.get(urlString, headers = headers)
+    
+    print("response.status_code : ", response.status_code)
 
     with open(file_path, 'r+', encoding='utf-8') as json_file:
         json.dump(response.json(), json_file, ensure_ascii=False, indent=4)
 
     data = response.json()
-    
-    print(data)
     
     return data
--- Character_Data_cache_async/main.py
+++ Character_Data_cache_async/main.py
@@ -9,19 +9,19 @@ from flask import Flask, request, jsonify
 import Union_Character_List
 
 api_key = "live_454c2b1ff9fd60b4ab2ee265c9f236ba3dfb7f486da0b6c3f76999ce002754e2efe8d04e6d233bd35cf2fabdeb93fb0d"
-character_name = "리마" #대 소문자를 구분지어야 된다.
+character_name = "이재욜" #대 소문자를 구분지어야 된다.
 world_name = "리부트"
 difficulty = "1"
 job = "모험가-전체 전직"
 page = "1"
 start_date = datetime(2023, 12, 22)
-end_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+end_date = datetime.now() - timedelta(days=1)
 
 # 캐릭터 기본 정보 조회(현재 기준)
-character_data = Character_basic_data.get_character_data(character_name, api_key, end_date)
+character_data = Character_basic_data.get_character_data(character_name, api_key, end_date.strftime('%Y-%m-%d'))
 character_nickname = character_data.get('character_name')
 ocid = Character_ocid.character_ocid(character_nickname, headers_data.headers_data(api_key))
-Union_Character_List.Union_Character_list(api_key, ocid, end_date)
+Union_Character_List.Union_Character_list(api_key, ocid, end_date.strftime('%Y-%m-%d'))
 
 if ocid is None:
     print("ocid is None")
